0d3f587978a33c980323b2af0449e646c724c3eb
mk124 <mk124a@gmail.com>
2013-07-22 03:45:38 +0800

    添加选车色对话框。
    Signed-off-by: mk124 <mk124a@gmail.com>

---
+++ src/main/java/net/gtaun/wl/vehicle/dialog/VehicleResprayDialog.java
@@ -0,0 +1,85 @@
+package net.gtaun.wl.vehicle.dialog;
+
+import net.gtaun.shoebill.Shoebill;
+import net.gtaun.shoebill.common.dialog.AbstractListDialog;
+import net.gtaun.shoebill.constant.VehicleModel;
+import net.gtaun.shoebill.data.Color;
+import net.gtaun.shoebill.event.dialog.DialogResponseEvent;
+import net.gtaun.shoebill.object.Player;
+import net.gtaun.shoebill.object.Vehicle;
+import net.gtaun.util.event.EventManager;
+import net.gtaun.wl.vehicle.VehicleManagerService;
+
+public class VehicleResprayDialog extends AbstractListDialog
+{
+	private final Vehicle vehicle;
+	private final VehicleManagerService vehicleManager;
+	private final int color1;
+	
+
+	public VehicleResprayDialog
+	(final Player player, final Shoebill shoebill, final EventManager eventManager, final Vehicle vehicle, final VehicleManagerService vehicleManager, final int start, final int end)
+	{
+		this(player, shoebill, eventManager, vehicle, vehicleManager, start, end, -1);
+	}
+	
+	public VehicleResprayDialog
+	(final Player player, final Shoebill shoebill, final EventManager eventManager, final Vehicle vehicle, final VehicleManagerService vehicleManager, final int start, final int end, final int color1)
+	{
+		super(player, shoebill, eventManager);
+		this.vehicle = vehicle;
+		this.vehicleManager = vehicleManager;
+		this.color1 = color1;
+		
+		if (vehicle == null)
+		{
+			destroy();
+			return;
+		}
+		
+		int modelId = vehicle.getModelId();
+		String name = VehicleModel.getName(modelId);
+		
+		String type = "主颜色";
+		if (color1 != -1) type = "子颜色";
+		
+		setCaption(String.format("%1$s: 选择 %2$s 的%3$s (模型: %4$d, HP: %5$1.0f％)", "车管", name, type, modelId, vehicle.getHealth()/10));
+		
+		for (int i=start; i<end; i++)
+		{
+			final int idx = i;
+			
+			String item = String.format("\t\t\t%1$s▇{FFFFFF} 颜色: %2$d %1$s▇", new Color(VehicleResprayGroupDialog.VEHICLE_COLOR_TABLE_RGBA[idx]).toEmbeddingString(), idx);
+			dialogListItems.add(new DialogListItem(item)
+			{
+				@Override
+				public void onItemSelect()
+				{
+					player.playSound(1083, player.getLocation());
+					
+					if (color1 == -1) new VehicleResprayGroupDialog(player, shoebill, eventManager, vehicle, vehicleManager, idx).show();
+					else player.getVehicle().setColor(color1, idx);
+					destroy();
+				}
+			});
+		}
+	}
+	
+	@Override
+	public void show()
+	{
+		super.show();
+	}
+	
+	@Override
+	protected void onDialogResponse(DialogResponseEvent event)
+	{
+		if (event.getDialogResponse() == 0)
+		{
+			player.playSound(1084, player.getLocation());
+			new VehicleResprayGroupDialog(player, shoebill, rootEventManager, vehicle, vehicleManager, color1).show();
+		}
+		
+		super.onDialogResponse(event);
+	}
+}
